281d1250aa157c841e971f09294a7ff07cca7e93
hsin <hsin.hung.li@nyu.edu>
2015-09-18 14:32:50 -0400

    add sim att

---
+++ sim_Att_divisive.m
@@ -0,0 +1,16 @@
+R1    = -1:.01:2;
+R2    = .25*ones(size(R1));
+R     = [R1;R2];
+sigma = .5;
+n     = 4;
+baseline = 1;
+
+aKernel = [1 -1; -1 1];
+aDrive  = abs(aKernel*R);
+aSign   = sign(aKernel*R);
+attGain = baseline + aDrive.^n ./ repmat((sum(aDrive.^n) + sigma^n),2,1) .*aSign;
+
+%figure;
+plot(R1-R2,attGain')
+xlabel('R1-R2')
+ylabel('Attentional gain factor')
