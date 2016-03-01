4f39c92f52b7449842ac82a4a3796e428c4487ff
Rachel Denison <r.n.denison@gmail.com>
2016-03-01 11:05:07 -0500

    n_model_FA major code cleanup. got rid of many previous bits of code that had been commented out.

--- n_model_FA.m
+++ n_model_FA.m
@@ -2,18 +2,12 @@ function [p] = n_model_FA(p)
 %This function is called by runModelFA.m
 
 idx = 1; %corresponds to t=0
-% istim = 1; % relevant only for WM. works only with no noise at the moment.
 for t = p.dt:p.dt:p.T
 %     counterdisp(t);
     idx = idx+1;
-%     if t > p.stimOnset + p.soa
-%         istim = 2;
-%     end
     
     %% Computing the responses of sensory layer
     %defining inputs (stimulus)
-    % use the stimulus that's been convolved with a temporal filter
-%     inp = p.e(:,idx);
     inp = p.stim(:,idx);
     
     %updating noise
@@ -21,9 +15,6 @@ for t = p.dt:p.dt:p.T
         (-p.d_n(:,idx-1) + randn(p.ntheta,p.nx)*p.d_noiseamp*sqrt(p.tau_n*2));
     
     %updating drives
-%     drive = halfExp(inp,p.p) .* p.att(:,idx-1) + p.d_n(:,idx);
-    %drive : stimulus_contrast * attention + noise
-%     p.d(:,idx) = halfExp(drive, 1); %rectification
     if isempty(p.rf) 
         drive = halfExp(inp,p.p); %%% leaving out w and a
     else
@@ -33,10 +24,7 @@ for t = p.dt:p.dt:p.T
             drive = zeros(p.ntheta,1);
         end
     end
-    p.d(:,idx) = halfExp(1+p.att(:,idx-1)).*drive; % previous -> turning
-%     into dynamical here:
-%     p.d(:,idx) = p.d(:,idx-1) + (p.dt/p.tau_e)*...
-%         (-p.d(:,idx-1) + halfExp(1+p.att(:,idx-1)).*drive);
+    p.d(:,idx) = halfExp(1+p.att(:,idx-1)).*drive;
     
     % normalization pool
     pool = p.d(:,idx);
@@ -50,7 +38,7 @@ for t = p.dt:p.dt:p.T
         (p.s(:,idx) + halfExp(sigma, p.p) + halfExp(p.a(:,idx-1)*p.wa, p.p)) ...
         + p.baselineMod;
     %Adaptation, p.a, is implemented as Wilson 2003 here. %Subject to change
-    %p.f{lay}(:,idx) = halfExp(p.f{lay}(:,idx)+ p.f_n{lay}(:,idx),1); %Add niose at the firing rate
+    %p.f(:,idx) = halfExp(p.f(:,idx)+ p.f_n(:,idx),1); %Add niose at the firing rate
     
     %update firing rates
     p.r(:,idx) = p.r(:,idx-1) + (p.dt/p.tau)*(-p.r(:,idx-1) + p.f(:,idx));
@@ -79,11 +67,6 @@ for t = p.dt:p.dt:p.T
         drive = halfExp(inp1,p.p); % rectify and raise to power
         p.dwm(:,idx,istim) = drive; % note difference from sensory layer: no attention
     end
-
-%     % with history
-%     drive = halfExp(p.r(:,idx),p.p); %%% leaving out w and a
-%     p.dwm(:,idx,:) = p.dwm(:,idx-1,:) + (p.dt/p.tau_dwm)*(-p.dwm(:,idx-1,:));
-%     p.dwm(:,idx,istim) = p.dwm(:,idx-1,istim) + (p.dt/p.tau_dwm)*(-p.dwm(:,idx-1,istim) + drive); % overwrite
     
     % normalization pool
     pool = p.dwm(:,idx,:); % pool over features and stimuli
@@ -117,21 +100,14 @@ for t = p.dt:p.dt:p.T
     end
     inp = [];
     for iPhase = 1:2
-%         inp0    = halfExp(r.*fliplr(p.aW(:,:,iPhase)), p.ap);
-%         inp(:,iPhase)     = sum(inp0(:,max(end-idx+2,1):end),2); % sum across time
         inp0 =  r.*fliplr(p.aW(:,:,iPhase)); % convolve step 1 (multiply)
         inp1 =  sum(inp0(:,max(end-idx+2,1):end),2)*p.dt; % convolve step 2 (integrate across time)
         inp(:,iPhase)     = halfExp(inp1,p.ap); % rectify and raise to power
     end
     aDrive  = inp*p.aKernel; % on channel - off channel
     aE      = halfExp(1 + p.attV(:,idx)*p.aMV).*(aDrive + p.aBaseline);
-%     aS      = repmat((sum(aDrive) + p.asigma^p.ap),p.ntheta,1); % S + sigma^2
     aS      = repmat((sum(abs(aE)) + p.asigma^p.ap),p.ntheta,1); % S + sigma^2
-    
-%     aDrive  = abs(p.aKernel*inp);
-%     aSign   = sign(p.aKernel*inp);
-%     attnGainI = halfExp(1 + p.aM*p.attV(:,idx), p.ap).* p.attV ./ repmat((sum(aDrive) + p.asigma^p.ap),p.ntheta,1) .*aSign,1);
-    
+
     attnGainI = aE./aS;
     p.attI(:,idx) = p.attI(:,idx-1) + (p.dt/p.tau_attI)*(-p.attI(:,idx-1) + attnGainI);
 
